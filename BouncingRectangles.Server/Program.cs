1476b9a731aa964efbb4eabd3e292ee1e5700b4c
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-26 22:56:11 +0700

    Added coordinates generator hosted service

--- BouncingRectangles.Server/Program.cs
+++ BouncingRectangles.Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddHostedService<CoordinatesGeneratorService>();
 
 var app = builder.Build();
 
---
+++ BouncingRectangles.Server/Services/CoordinatesGeneratorService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BouncingRectangles.Server.Services
+{
+    public class CoordinatesGeneratorService : IHostedService
+    {
+        private readonly ILogger<CoordinatesGeneratorService> _logger;
+        private readonly TimeSpan _generateInterval = TimeSpan.FromSeconds(1);
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public CoordinatesGeneratorService(ILogger<CoordinatesGeneratorService> logger)
+        {
+            _logger = logger;
+        }
+
+        private async Task GenereateCoordinates(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                try
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    // Generate coordinates
+
+                    await Task.Delay(_generateInterval, cancellationToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, null);
+                }
+            }
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            var rnd = new Random();
+            var tasksCount = rnd.Next(Environment.ProcessorCount);
+            var token = _cancellationTokenSource.Token;
+            for (int i = 0; i < tasksCount; i++)
+            {
+                Task.Run(() => GenereateCoordinates(token), token);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+
+            return Task.CompletedTask;
+        }
+    }
+}
