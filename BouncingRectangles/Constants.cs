fd6afab1e683a7f599b578f8605ab865cee4fee4
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-28 18:36:11 +0700

    Made common library

---
+++ BouncingRectangles/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace BouncingRectangles.Common
+{
+    public static class Constants
+    {
+        public static int FieldWidth { get; } = 500;
+        public static int FieldHeight { get; } = 500;
+    }
+}
