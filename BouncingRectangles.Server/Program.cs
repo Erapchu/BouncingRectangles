7de9cc3a773671788ceacb331ab82f9eb11325f6
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-28 20:36:35 +0700

    Implemented Grpc service subscription logic

--- BouncingRectangles.Server/Program.cs
+++ BouncingRectangles.Server/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddSingleton<ICoordinatesGeneratorService, CoordinatesGeneratorService>();
 builder.Services.AddSingleton<IRectangleFactory, RectangleFactory>();
+builder.Services.AddSingleton<IRectangleSubscriberFactory, RectangleSubscriberFactory>();
+builder.Services.AddTransient<IRectangleSubscriber, RectangleSubscriber>();
 
 var app = builder.Build();
 
--- BouncingRectangles.Server/Services/BouncingRectangeGrpcService.cs
+++ BouncingRectangles.Server/Services/BouncingRectangeGrpcService.cs
@@ -1,17 +1,72 @@
 ﻿using BouncingRectangles.Protos;
+using BouncingRectangles.Server.Models;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BouncingRectangles.Server.Services
 {
     public class BouncingRectangeGrpcService : Protos.BouncingRectangeHost.BouncingRectangeHostBase
     {
-        public override Task Subscribe(
-            SubscribeRequest request,
-            IServerStreamWriter<BouncingRectangleUpdate> responseStream,
+        private readonly IRectangleSubscriberFactory _rectangleSubscriberFactory;
+        private readonly ILogger<BouncingRectangeGrpcService> _logger;
+
+        public BouncingRectangeGrpcService(
+            IRectangleSubscriberFactory rectangleSubscriberFactory,
+            ILogger<BouncingRectangeGrpcService> logger)
+        {
+            _rectangleSubscriberFactory = rectangleSubscriberFactory;
+            _logger = logger;
+        }
+
+        public override async Task Subscribe(
+            SubscribeRequestDto request,
+            IServerStreamWriter<BouncingRectangleUpdateDto> responseStream,
             ServerCallContext context)
         {
-            return base.Subscribe(request, responseStream, context);
+            var subscriber = _rectangleSubscriberFactory.GetSubscriber();
+            subscriber.Update += async (sender, e) =>
+                await WriteUpdateAsync(responseStream, e.Rectangles);
+
+            _logger.LogInformation("Subscription started");
+            // Just awaits for client closes the connection/interrupted somehow
+            await WaitForCancellation(context.CancellationToken);
+            subscriber.Dispose(); // Dispose subscriber
+            _logger.LogInformation("Subscription finished");
+        }
+
+        private async Task WriteUpdateAsync(IServerStreamWriter<BouncingRectangleUpdateDto> stream, IEnumerable<Rectangle> rectangles)
+        {
+            try
+            {
+                var bouncingRectangleUpdate = new BouncingRectangleUpdateDto();
+                foreach (var rect in rectangles)
+                {
+                    bouncingRectangleUpdate.Rectangles.Add(new BouncingRectangleDto()
+                    {
+                        Height = rect.Height,
+                        Width = rect.Width,
+                        X = rect.X,
+                        Y = rect.Y,
+                        Id = rect.Id.ToString(),
+                    });
+                }
+                await stream.WriteAsync(bouncingRectangleUpdate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to write message: {ex.Message}");
+            }
+        }
+
+        private static Task WaitForCancellation(CancellationToken token)
+        {
+            var completion = new TaskCompletionSource<object>();
+            token.Register(() => completion.SetResult(null));
+            return completion.Task;
         }
     }
 }
