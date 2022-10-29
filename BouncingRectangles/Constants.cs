d8c7583f9a4128ab01ed51852ad14ae0547046a2
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-29 08:53:46 +0700

    Removed unused folder

--- BouncingRectangles/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BouncingRectangles.Common
-{
-    public static class Constants
-    {
-        public static int FieldWidth { get; } = 500;
-        public static int FieldHeight { get; } = 500;
-    }
-}
